344edd6f64115369b05b81a199da30f8c3c3d283
Abhijit <abhibmatsagar@gmail.com>
2023-06-07 11:53:32 +0530

    Footer App.jsx error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import { fetchDataFromApi } from "./utils/api";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import PageNotFound from "./pages/404/PageNotFound";
 import Details from "./pages/details/Details"
