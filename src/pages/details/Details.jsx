4e22125f094b5b2492c51413ed35fa0539cbe072
Abhijit <abhibmatsagar@gmail.com>
2023-06-07 12:14:19 +0530

    hooks folder problem resolved

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+import useFetch from "../../hooks/useFetch.jsx";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import "./style.scss";
 import Cast from "./cast/Cast";
