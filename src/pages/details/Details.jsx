0aa87f05426a5f842c581a64058ba218afc3d3bc
Abhijit <abhibmatsagar@gmail.com>
2023-06-07 12:09:14 +0530

    hooks folder name changed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import useFetch from "../../Hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import "./style.scss";
 import Cast from "./cast/Cast";
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -4,7 +4,7 @@ import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
-import useFetch from "../../../Hooks/useFetch";
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
