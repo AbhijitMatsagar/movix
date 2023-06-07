d50b89289335094dd45435c0f86f460054c4667a
Abhijit <abhibmatsagar@gmail.com>
2023-06-07 11:22:40 +0530

    error fixed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,13 @@
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import "./style.scss";
 
 const PageNotFound = () => {
   return (
-    <div>
-      
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
   )
 }
